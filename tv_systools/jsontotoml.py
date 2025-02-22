1fdd877e7442557eceb42cdd4d8ab65440021db9
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2025-02-22 17:37:15 +0100

    rudimentary json2toml conversion

---
+++ tv_systools/jsontotoml.py
@@ -0,0 +1,50 @@
+import json
+import toml
+from typer import Typer
+from pathlib import Path
+
+app = Typer()
+
+
+def denull(data, replacement=None):
+    if isinstance(data, dict):
+        result = {}
+        for key, value in data.items():
+            if value is None:
+                if replacement is not None:
+                    result[key] = replacement
+            else:
+                result[key] = denull(value, replacement)
+        return result
+    elif isinstance(data, list):
+        result = []
+        for item in data:
+            if data is None:
+                if replacement is not None:
+                    result.append(replacement)
+            else:
+                result.append(denull(item, replacement))
+        return result
+    elif data is None:
+        if replacement is not None:
+            return replacement
+        else:
+            raise ValueError("Cannot have None value in data")
+    else:
+        return data
+
+
+@app.command()
+def convert(json_file: Path, toml_file: Path, null_replacement=None):
+    with open(json_file, "r") as f:
+        data = json.load(f)
+    if isinstance(data, list):
+        if len(data) == 1:
+            data = data[0]
+        else:
+            data = {json_file.stem: "data"}
+
+    data = denull(data, null_replacement)
+
+    with open(toml_file, "w") as f:
+        toml.dump(data, f)
