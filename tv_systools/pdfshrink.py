0f2cb2228f386b7f126eca8ef5beae3af8458def
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2025-11-24 12:11:12 +0100

    fix(pdfshrink): copy when path.replace does not work (due to different filesystems)

--- tv_systools/pdfshrink.py
+++ tv_systools/pdfshrink.py
@@ -1,4 +1,10 @@
-from rich.progress import BarColumn, ProgressColumn, MofNCompleteColumn, Task, TextColumn
+from rich.progress import (
+    BarColumn,
+    ProgressColumn,
+    MofNCompleteColumn,
+    Task,
+    TextColumn,
+)
 from typing import Optional
 from rich.table import Column
 from rich.text import Text
@@ -10,7 +16,7 @@ from .util import configure_logging
 from os import fspath, process_cpu_count
 from pathlib import Path
 import shutil
-from cyclopts import App, Parameter, validators
+from cyclopts import App, Parameter
 from humanize import naturalsize
 from rich import print
 from tempfile import NamedTemporaryFile
@@ -22,9 +28,16 @@ logger = logging.getLogger(__name__)
 app = App()
 app.register_install_completion_command(add_to_startup=False)
 
-class ShrinkResult:
 
-    def __init__(self, success: bool, original: Path, compressed: Path, output: Path | None = None, tolerance: float = 0.01):
+class ShrinkResult:
+    def __init__(
+        self,
+        success: bool,
+        original: Path,
+        compressed: Path,
+        output: Path | None = None,
+        tolerance: float = 0.01,
+    ):
         self.success = success
         self.original = original
         self.compressed = compressed
@@ -33,7 +46,11 @@ class ShrinkResult:
 
         self.original_size = original.stat().st_size
         self.output_size = compressed.stat().st_size
-        self.max_tolerable_size = int(self.original_size * (1 - self.tolerance)) if self.tolerance < 1 else self.original_size - int(self.tolerance)
+        self.max_tolerable_size = (
+            int(self.original_size * (1 - self.tolerance))
+            if self.tolerance < 1
+            else self.original_size - int(self.tolerance)
+        )
 
     @property
     def improved(self) -> bool:
@@ -63,32 +80,54 @@ class ShrinkResult:
 
     def finalize(self):
         if self.improved:
-            self.compressed.replace(self.output or self.original)
+            try:
+                self.compressed.replace(self.output or self.original)
+            except OSError as e:
+                logging.debug(
+                    "Failed to move compressed file %s into place, trying copy+delete: %s",
+                    self.compressed,
+                    e,
+                )
+                shutil.copy2(self.compressed, self.output or self.original)
+                self.compressed.unlink()
 
 
-async def shrink_file(source: Path, output: Path | None = None, tolerance: float = 0.01) -> ShrinkResult:
+async def shrink_file(
+    source: Path, output: Path | None = None, tolerance: float = 0.01
+) -> ShrinkResult:
     gs = shutil.which("gs")
     if gs is None:
         raise OSError("Ghostscript (gs) not found in PATH")
-    with NamedTemporaryFile(delete_on_close=False, suffix=".pdf", prefix=f".{source.stem}__") as tempfile:
+    with NamedTemporaryFile(
+        delete_on_close=False, suffix=".pdf", prefix=f".{source.stem}__"
+    ) as tempfile:
         tempfile.close()
         compressed = Path(tempfile.name)
-        exitcode = await aexecute([
-            gs,
-            f"-o{fspath(compressed)}",
-            "-sDEVICE=pdfwrite",
-            "-dCompatibilityLevel=1.4",
-            "-dPDFSETTINGS=/screen",
-            # "-dQUIET",
-            "-dNOPAUSE",
-            fspath(source),
-        ], prefix=source.name + ": ", stdout_level=logging.DEBUG)
-        result = ShrinkResult(exitcode == 0, source, compressed, output, tolerance=tolerance)
+        exitcode = await aexecute(
+            [
+                gs,
+                f"-o{fspath(compressed)}",
+                "-sDEVICE=pdfwrite",
+                "-dCompatibilityLevel=1.4",
+                "-dPDFSETTINGS=/screen",
+                # "-dQUIET",
+                "-dNOPAUSE",
+                fspath(source),
+            ],
+            prefix=source.name + ": ",
+            stdout_level=logging.DEBUG,
+        )
+        result = ShrinkResult(
+            exitcode == 0, source, compressed, output, tolerance=tolerance
+        )
         result.finalize()
         return result
 
+
 class RenderableExtraColumn(ProgressColumn):
-    def __init__(self, extra_key: str, default: str = "", table_column: Optional[Column] = None):
+    def __init__(
+        self, extra_key: str, default: str = "", table_column: Optional[Column] = None
+    ):
         super().__init__(table_column=table_column)
         self.extra_key = extra_key
         self.default = default
@@ -96,15 +135,17 @@ class RenderableExtraColumn(ProgressColumn):
     def render(self, task: Task) -> Text:
         return task.fields.get(self.extra_key, Text(self.default))
 
+
 @app.default
 async def pdfshrink(
-        sources: list[Path],
-        /, *,
-        tolerance: Annotated[float, Parameter(alias="t")] = 0.01,
-        parallel: Annotated[int, Parameter(alias="-p")] = process_cpu_count() or 8,
-        largest_first: Annotated[bool, Parameter(alias="-l")] = True,
-        verbose: Annotated[bool, Parameter(alias="-v", negative=False)] = False,
-        debug: Annotated[bool, Parameter(alias="-vv", negative=False)] = False,
+    sources: list[Path],
+    /,
+    *,
+    tolerance: Annotated[float, Parameter(alias="t")] = 0.01,
+    parallel: Annotated[int, Parameter(alias="-p")] = process_cpu_count() or 8,
+    largest_first: Annotated[bool, Parameter(alias="-l")] = True,
+    verbose: Annotated[bool, Parameter(alias="-v", negative=False)] = False,
+    debug: Annotated[bool, Parameter(alias="-vv", negative=False)] = False,
 ):
     """
     Try to shrink all given PDF files using Ghostscript.
@@ -117,14 +158,24 @@ async def pdfshrink(
         verbose: produce verbose output
         debug: even more vrbose output, including GhostScripts messages
     """
-    configure_logging(level=logging.DEBUG if debug else (logging.INFO if verbose else logging.WARNING))
+    configure_logging(
+        level=logging.DEBUG if debug else (logging.INFO if verbose else logging.WARNING)
+    )
     results: list[ShrinkResult] = []
     if largest_first:
         sources = sorted(sources, key=lambda p: p.stat().st_size, reverse=True)
 
-    with Progress(TextColumn("Shrinking PDFs"), BarColumn(), MofNCompleteColumn(), RenderableExtraColumn('result'), transient=True) as progress:
+    with Progress(
+        TextColumn("Shrinking PDFs"),
+        BarColumn(),
+        MofNCompleteColumn(),
+        RenderableExtraColumn("result"),
+        transient=True,
+    ) as progress:
         progress_task = progress.add_task("Shrinking PDFs...", total=len(sources))
-        async for result in map_unordered(partial(shrink_file, tolerance=tolerance), sources, limit=parallel):
+        async for result in map_unordered(
+            partial(shrink_file, tolerance=tolerance), sources, limit=parallel
+        ):
             progress.update(progress_task, advance=1, result=result)
             logger.info("%s", result)
             results.append(result)
@@ -133,4 +184,6 @@ async def pdfshrink(
     total_improvement = sum(r.improvement for r in results)
     n_improved = sum(r.improved for r in results)
 
-    print(f"Shrunk {n_improved} of {len(sources)} files, winning {naturalsize(total_improvement)} of {naturalsize(total_orig)} ({total_improvement / total_orig:2.1%})")
+    print(
+        f"Shrunk {n_improved} of {len(sources)} files, winning {naturalsize(total_improvement)} of {naturalsize(total_orig)} ({total_improvement / total_orig:2.1%})"
+    )
